dcc557f4f42e2ec9b373bd74238119e95d602331
bathant702 <bathant702nv@gmail.com>
2023-04-05 16:34:37 -0700

    odified scroll buttons and changed layout. added mouseover event and setting up to do overlay menu

--- Portfolio/JS/app.js
+++ Portfolio/JS/app.js
@@ -1,27 +1,48 @@
-// Scroll button
+
+// Scroll button home
 const scrollBtnHome = document.getElementById("scroll-top-btn-home");
 scrollBtnHome.addEventListener("click", scrollToTop);
 // scroll functions
-function scrollToTop() {
+function scrollToTop(){
   window.scrollTo({
     top: 0,
     behavior: "smooth",
   });
 }
 
-// Scroll button
+// Scroll button Down
 const scrollBtnDown = document.getElementById("scroll-top-btn-down");
-scrollBtnDown.addEventListener("click", scrollToTop);
-// scroll functions
-function scrollToTop() {
-  window.scrollTo({
-    top: -100,
-    behavior: "smooth",
-  });
-}
 
+scrollBtnDown.addEventListener("click", function () {
+  // #bio
+  // #resume
+  // #resources
+  const bioSection = document.getElementById("bio");
+  const resumeSection = document.getElementById("resume");
+  const resourcesSection = document.getElementById("resources");
 
-// Dropdown menu
+  if (window.scrollY < bioSection.offsetTop) {
+    //scroll to bio section if not visible
+    window.scrollTo({
+      top: bioSection.offsetTop,
+      behavior: "smooth",
+    });
+  } else if (window.scrollY < resumeSection.offsetTop) {
+    //scroll to next section
+    window.scrollTo({
+      top: resumeSection.offsetTop,
+      behavior: "smooth",
+    });
+  } else {
+    //scroll to next section
+    window.scrollTo({
+      top: resourcesSection.offsetTop,
+      behavior: "smooth",
+    });
+  }
+});
+
+// Dropdown menu -- changing. will be doing an overlay menu
 //make variable of the IDs you need to grab
 const dropdownBtn = document.getElementById("dropdownBtn");
 const dropdownMenu = document.getElementById("dropdownMenu");
@@ -42,13 +63,16 @@ linksArray.forEach((singleLink) => {
 });
 
 //Links mouseover
-document.getElementById("demo").addEventListener("mouseover", mouseOver);
-document.getElementById("demo").addEventListener("mouseout", mouseOut);
+document.getElementById("project-link-1").addEventListener("mouseover", mouseOver);
+document.getElementById("project-link-2").addEventListener("mouseover", mouseOver);
+document.getElementById("project-link-3").addEventListener("mouseover", mouseOver);
+// document.getElementById("project-link-1").addEventListener("mouseout", mouseOut);
 
 function mouseOver() {
-  document.getElementById("demo").style.color = "red";
+  document.getElementById("project-link-1").style.color = "red";
+  document.getElementById("project-link-2").style.color = "green";
+  document.getElementById("project-link-3").style.color = "yellow";
 }
 
-function mouseOut() {
-  document.getElementById("demo").style.color = "black";
-}
+// current mouse over is working as intended, but its also firing them all at once and is not smooth by and degree. will look into this further tomorrow
+
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # portfolio
 Portfolio Site
 
-My first portfolio site. I'll be focusing on creating a standard portfolio with interactive javascript pieces.
+This is my first portfolio site. Throughout the GA course (and most likely after), I'll be adding on and modifying the contents of this site to best reflect my capabilities as a fullstack developer. My portfolio will have the following information:
+- Bio - who I am
+- Resume - my professional and technical creditials as well as projects I am currently working on or have finished
+- Links - to resources I use for inspiration and help as well as to my hobbies and interests.
+
+This readme is a record of my progress.
+
+Log 4.5.2023 @ 3:16pm
+- added up and down scroll buttons.
+- changed the dropdown menu for an overlay menu.
+- got a layout for a hover with links
